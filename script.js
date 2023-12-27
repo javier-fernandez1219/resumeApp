80fbbab3c526dd16260203046c9c5c4dd347cf43
Javier Fernandez <jafernandez1219@gmail.com>
2023-12-27 16:54:09 -0500

    added skills
    added skills,

--- script.js
+++ script.js
@@ -2,9 +2,11 @@ document.getElementById("color-mode-checkbox").addEventListener("change", functi
     if (this.checked) {
         document.getElementById("myDiv").style.backgroundImage = 'None'
         document.getElementById("myDiv").style.backgroundColor = 'white'
+        document.getElementById("myDiv").style.color = 'black'
     }else{
         document.getElementById("myDiv").style.backgroundImage = 'linear-gradient(to right, #e66465, #090f53)'
         document.getElementById("myDiv").style.backgroundColor = 'black'
+        document.getElementById("myDiv").style.color = 'white'
     }
     
 }
